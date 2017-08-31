b585ceff455f9a043c1b4bfe695111effcba13b1
Ronald Quenta <ronald.otn@gmail.com>
2017-08-31 15:16:20 -0400

    add bootstrap

--- src/Base/Layouts/views/dynaform.blade.php
+++ src/Base/Layouts/views/dynaform.blade.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        <script>
-            var structure = JSON.parse('{!! $structure !!}');
-        </script>
-    </head>
-    <body>
-        <div id="app"></div>
-        <script src="/lib/js/build.js"></script>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>nextform</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+    <style>
+        body {
+            padding: 16px;
+        }
+    </style>
+</head>
+<body>
+<div id="app"></div>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+<script>
+    var structure = JSON.parse('{!! $structure !!}');
+</script>
+<script src="/lib/js/build.js"></script>
+</body>
 </html>
